6a1294f370e4d43e37229072d12cf44a9dd97a01
Huss <hussein@joincoded.com>
2017-10-09 12:34:29 +0300

    Update/edit view completed

--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,8 @@ from posts import views
 
 urlpatterns = [
 url(r'^create/$', views.post_create, name="create"),
-url(r'^detail/(?P<post_id>\d+)$', views.post_detail, name="detail"),
+url(r'^detail/(?P<post_id>\d+)/$', views.post_detail, name="detail"),
 url(r'^list/$', views.post_list, name="list"),
-url(r'^update/$', views.post_update, name='update'),
+url(r'^update/(?P<post_id>\d+)/$', views.post_update, name='update'),
 url(r'^delete/$', views.post_delete, name='delete'),
 ]
