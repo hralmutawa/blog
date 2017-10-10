3b1238a2611beb072443f788037835b4c4958984
Huss <hussein@joincoded.com>
2017-10-10 13:30:44 +0300

    Adding draft and publishing to posts

--- posts/models.py
+++ posts/models.py
@@ -16,6 +16,9 @@ class Post(models.Model):
 	content = models.TextField()
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+	draft = models.BooleanField(default=False)
+	publish = models.DateTimeField(auto_now=False, auto_now_add=False)
+	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
 	def __str__(self):
 		return self.title
